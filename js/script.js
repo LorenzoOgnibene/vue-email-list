986f619b9263922b464fc75c486cb107f5428bba
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-12-21 15:05:45 +0100

    Created a list of 10 random email.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            randomEmail : [],
+            
+    }
+    },
+
+    methods:{
+           getRandomEmail(){
+            for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data);
+                    this.randomEmail.push(response.data)
+                })
+               }
+               console.log(this.randomEmail)
+            }
+        
+    },
+    created(){
+        this.getRandomEmail();
+    }
+}).mount('#app')
